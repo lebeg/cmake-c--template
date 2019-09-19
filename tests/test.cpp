a749c4b727537b49e3809c6093a49cf32c6e017f
Anton Chernov <mechernov@gmail.com>
2019-09-19 16:05:53 +0200

    Refactord cmake tests

---
+++ tests/test.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <stdint.h>
+
+#include "gtest/gtest.h"
+
+struct TestRow {
+    std::string test_case;
+    std::int32_t test_result;
+};
+
+std::ostream &operator<<(std::ostream &out, const TestRow &c) {
+    out << c.test_case << " = " << c.test_result;
+    return out;
+}
+
+class ArithmeticTest : public ::testing::TestWithParam<TestRow> {
+
+};
+
+
+TEST_P(ArithmeticTest, ProducesCorrectResults
+) {
+    auto row = GetParam();
+    EXPECT_EQ(true, true);
+}
+
+INSTANTIATE_TEST_CASE_P
+
+(CorrectCases, ArithmeticTest,
+ ::testing::Values(TestRow{"4", 4}, TestRow{"1+2+3+4+5", 15},
+                   TestRow{"3+2", 5},
+                   TestRow{"(5-8)*3", -9},
+                   TestRow{"  ( 2 + 8) ", 10},
+                   TestRow{" ( ( 2 + 4) * 8) ", 10})
+);
